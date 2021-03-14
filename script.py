b4d03a3625c2d1bbe1a66a12b6784d4dcb4a7c3c
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-03-14 17:28:10 -0400

    creating a trim whitespace function

--- script.py
+++ script.py
@@ -6,3 +6,14 @@ with open('people.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     with open('state_to_region.json') as f:
         data = json.load(f)
+
+    def trimList(list):
+        currentElement = 0
+        for element in list:
+            list[currentElement] = element.strip()
+            currentElement += 1
+        return list
+
+    for list in readCSV:
+        list = copy.deepcopy(trimList(list))
+        print(list)
