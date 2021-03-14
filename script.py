97fa3293a8b4bb3f1a36f09e398818eae4b14ea5
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-03-14 17:37:27 -0400

    retrieving region for rows with 5 elements

--- script.py
+++ script.py
@@ -16,4 +16,9 @@ with open('people.csv') as csvfile:
 
     for list in readCSV:
         list = copy.deepcopy(trimList(list))
-        print(list)
+
+        if (len(list) == 5):
+            for i in range(0, 5):
+                if (list[i] in data.keys()):
+                    # Assuming that list[i] is in position 3 when row length = 5
+                    list[i + 1] = data[list[i]]
