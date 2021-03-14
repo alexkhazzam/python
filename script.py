c15408a267d330a3db3a49ef9e481caccaa68f24
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-03-14 17:25:32 -0400

    storing file contents in variable

--- script.py
+++ script.py
@@ -1,3 +1,8 @@
 import csv
 import copy
 import json
+
+with open('people.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    with open('state_to_region.json') as f:
+        data = json.load(f)
