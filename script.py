818e222aa8232669bcca2f18794af90ff40ef562
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-03-15 16:41:18 -0400

    refactoring code

--- script.py
+++ script.py
@@ -1,46 +1,48 @@
+import sqlite3
 import csv
-import copy
 import json
 
 editedLists = []
 
 with open('people.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
+    readcsv = csv.reader(csvfile, delimiter=',')
+
     with open('state_to_region.json') as f:
         data = json.load(f)
 
     def trimList(list):
-        currentElement = 0
-        for element in list:
-            list[currentElement] = element.strip()
-            currentElement += 1
+        for i in range(0, len(list)):
+            list[i] = list[i].strip()
         return list
 
-    def elementExists(el):
-        return el in data.keys()
+    rPos = 4
+    sPos = 3
 
-    for list in readCSV:
-        list = copy.deepcopy(trimList(list))
+    for l in readcsv:
+        l = trimList(l)
 
-        if (len(list) == 5):
-            for i in range(0, 5):
-                # Assuming that list[i] is in position 3 when row length = 5
-                if (elementExists(list[i])):
-                    list[i + 1] = data[list[i]]
+        def regionExists(pos=3):
+            return (l[pos] in data.keys())
+
+        if (len(l) == 5):
+            if (regionExists() and (l[rPos] == '')):
+                l[rPos] = data[l[sPos]]
         else:
-            while (len(list) != 5):
-                list.append('')
-            for i in range(0, 5):
-                if (elementExists(list[i])):  # Not so efficient...but it will work
-                    list[4] = data[list[i]]
-                    list[3] = list[i]
-                    list[i] = None
-                    break  # No need to keep looping once state found
-                # If state not found => continue and do not touch row
+            print(l)
+            while(len(l) != 5):
+                l.append('NULL')
+            for i in range(0, len(l)):
+                if (regionExists(i)):
+                    l[rPos] = data[l[i]]
+                    l[sPos] = l[i]
+                    l[i] = 'NULL'
+                    break
+                if (l[i] == ''):
+                    l[i] = 'NULL'
 
-        editedLists.append(list)
+        editedLists.append(l)
 
 with open('people.csv', 'w') as csvfile:
-    csvWriter = csv.writer(csvfile)
-    for list in editedLists:
-        csvWriter.writerow(list)
+    csvwriter = csv.writer(csvfile)
+    for i in editedLists:
+        csvwriter.writerow(i)
